cad8288d5b41ea41e07231a8e54b6ab4352a1b08
antonio lugogo <antoniolexis2001@gmail.com>
2022-08-30 12:20:15 +0100

    4º fase

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,9 +2,15 @@ export default {
     app:3300,
 
     mySQLConnection: {
-        
-
-
+          client: 'mysql',
+            connection: {
+                host : '127.0.0.1',
+                port : 3306,
+                user : 'admin',
+                password : '',
+                database : 'bibliotecaBantu'
+            }
+                    
     }
 
 
