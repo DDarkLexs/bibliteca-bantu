d6cd474d6123ab8ad4bd3d28c0342edffb251f23
DDark lexs <antoniolexis2001@gmail.com>
2022-09-21 01:12:55 +0100

    nº fase

--- src/controllers/translate/index.js
+++ src/controllers/translate/index.js
@@ -6,6 +6,9 @@ import knex from '../../models'
 import md5 from 'md5'
 import { portuguesParaChokwe,
     portuguesParaUmbundu  } from '../../../enviroment/data.json'
+import { ptAndchokweAndumbundu } from '../../../enviroment/newData.json'
+
+
 
 export default  {
 
@@ -83,13 +86,12 @@ export default  {
         try {
             const translation = (await knex.raw(`
             SELECT 
-            pt.palavra as portugues, 
-            un.palavra as umbundu
+            pt.palavra AS portugues, 
+            un.palavra AS umbundu
             FROM portugues pt
             INNER join traduzido t ON pt.id_pt = t.id_pt
             INNER join umbundu un ON un.id_umbundu = t.id_umbundu
-            where pt.palavra = '${req.query.palavra}'
-            ;
+            WHERE pt.palavra = '${req.query.palavra}';
             `))[0][0]
 
             console.log(translation)
@@ -107,7 +109,33 @@ export default  {
         }
             
     },
+    async portugueseToUmbunduAndChokwe(req,res,next){
+        try {
+            const translation = (await knex.raw(`
+            SELECT 
+            pt.palavra AS portugues, 
+            un.palavra AS umbundu
+            FROM portugues pt
+            INNER join traduzido t ON pt.id_pt = t.id_pt
+            INNER join umbundu un ON un.id_umbundu = t.id_umbundu
+            WHERE pt.palavra = '${req.query.palavra}';
+            `))[0][0]
 
+            console.log(translation)
+
+
+            res.json({ response:true, translation })
+    
+        
+        } catch (error) {
+
+            res.json({ response:false, msg: error })
+               
+
+            
+        }
+            
+    },
 
     async insertportugueseToUmbundu(req,res,next){
         try {
@@ -149,8 +177,8 @@ export default  {
     async insertAll(req,res,next){
         try {
 
-            for (let i = 0; i < portuguesParaChokwe.length; i++) { // teste
-                const { chokwe,portugues } = portuguesParaChokwe[i]; // teste
+            for (let i = 0; i < ptAndchokweAndumbundu.length; i++) { // teste
+                const { chokwe,portugues,umbundu } = ptAndchokweAndumbundu[i]; // teste
 
                 // const {  portugues , chokwe } = req.query
                 
@@ -161,32 +189,8 @@ export default  {
                 .insert({palavra:chokwe , codigo:md5(chokwe) })
                 
                 const id_umbundu = await knex('umbundu')
-                .insert({palavra:null , codigo:md5('nulo') })
-               
-                const id_traduzido = await knex('traduzido')
-                .insert({
-                    significado:'',
-                    descricao:'',
-                    id_pt: id_pt[0],
-                    id_umbundu: id_umbundu[0],
-                    id_chokwe:id_chokwe[0]
-                })
-                    
-            }
-
-            for (let i = 0; i < portuguesParaUmbundu.length; i++) { // teste
-                const { umbundu , portugues } = portuguesParaUmbundu[i]; // teste
-
-                // const {  portugues , umbundu } = req.query
-                const id_pt = await knex('portugues')
-                .insert({ palavra:portugues , codigo:md5(portugues) })
-                
-                const id_umbundu = await knex('umbundu')
                 .insert({palavra:umbundu , codigo:md5(umbundu) })
                
-                const id_chokwe = await knex('chokwe')
-                .insert({palavra:null , codigo:md5('nulo') })
-                
                 const id_traduzido = await knex('traduzido')
                 .insert({
                     significado:'',
@@ -195,7 +199,7 @@ export default  {
                     id_umbundu: id_umbundu[0],
                     id_chokwe:id_chokwe[0]
                 })
-                
+                    console.log(`${id_traduzido} -> ${portugues} = ${chokwe} = ${umbundu}`)                    
             }
 
 
--- src/routes/translate/index.js
+++ src/routes/translate/index.js
@@ -8,19 +8,25 @@ export default (app) => {
 
     app.route('/translate/inserirTodo')
     .post((req, res, next) => {
-        console.log('portugues para umbundu e umbuntu todo!')
+        console.log('Inserindo de portugues para chokwe e umbuntu!')
         
         next()
 
     },tMiddleware.insertAll)
 
-
+    app.route('/translate/portugueseToUmbunduAndChokwe')
+    .get((req, res, next) => {
+        console.log('portugues para umbundu e chokwe')
+        next()
+    },tMiddleware.portugueseToUmbunduAndChokwe)
+    
 
     app.route('/translate/portugueseToUmbundu')
     .get((req, res, next) => {
         console.log('portugues para umbundu')
         next()
     },tMiddleware.portugueseToUmbundu)
+
     .post((req,res,next) => {
         console.log(`inserir de portuguese para Umbundu`)
         next()
